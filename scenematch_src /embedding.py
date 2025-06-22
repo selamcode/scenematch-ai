19d38706841851513b6a680e80fd7cc6d889333b
selamcode <skcode8@gmail.com>
2025-06-22 16:41:32 -0700

    embedding error fixed - embedding working

--- scenematch_src /embedding.py
+++ scenematch_src /embedding.py
@@ -1,7 +1,9 @@
 
-from qdrant_client import QdrantClient
+from qdrant_client import QdrantClient, models
 from qdrant_client.models import PointStruct, Document
 from io_utils import load_json
+
+
 def prepare_points(docs: list[dict]) -> list[PointStruct]:
     points = []
     for doc in docs:
@@ -13,18 +15,18 @@ def prepare_points(docs: list[dict]) -> list[PointStruct]:
             PointStruct(
                 id=doc["id"],
                 vector={
-                    "overview": Document(
+                    "overview_dense": Document(
                         text=overview,
                         model="jinaai/jina-embeddings-v2-small-en",
                     ),
-                    "tagline": Document(
+                    "tagline_dense": Document(
                         text=tagline,
                         model="jinaai/jina-embeddings-v2-small-en",
                     ),
-                    "bm25": Document(
+                    "genre_sparse_bm25": Document(
                         text=" ".join(genres),
                         model="Qdrant/bm25",
-                    ),
+                    )
                 },
                 payload={
                     "title": doc.get("title"),
@@ -37,10 +39,12 @@ def prepare_points(docs: list[dict]) -> list[PointStruct]:
     return points
 
 def upsert_points(client: QdrantClient, collection_name: str, points: list[PointStruct]) -> None:
+    
     client.upsert(
         collection_name=collection_name,
         points=points,
     )
+    print(f"Inserted {len(points)} points into '{collection_name}'.")
 
 def embed(collection_name: str, json_file: str, client: QdrantClient) -> None:
     docs = load_json(json_file)
