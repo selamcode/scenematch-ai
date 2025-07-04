ae8c6925ac20b26aaab8ee5146e2e4c0bb45729a
selamcode <skcode8@gmail.com>
2025-07-03 22:41:20 -0700

    Stop tracking chat_gui.py and ignore it

--- scenematch/chat_app.py
+++ scenematch/chat_app.py
@@ -33,4 +33,4 @@ def chat():
     return render_template("chat.html", chat_history=chat_history)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5050)
+    app.run(debug=True, host="127.0.0.1", port=5000)
